70817733944028aa25d735f5fba3a4406eee05da
0xAliRaza <0xAliRaza@users.noreply.github.com>
2022-02-02 05:34:25 +0000

    deploy: 648f42441ff524019616ec7a8aead0cb13698b66

---
+++ _nuxt/static/1643780064/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643780064",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4d084a82"}}}}(null,"\u002F"));
