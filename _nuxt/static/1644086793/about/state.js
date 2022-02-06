c88e42b9275ca16a8aac986475b1bf1d439abe6a
0xAliRaza <0xAliRaza@users.noreply.github.com>
2022-02-06 17:11:14 +0000

    deploy: 0bcb6aae6fcb654349b6e218796784bb0690693a

--- _nuxt/static/1644086793/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644086793",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"07e785a8"}}}}(null));
--- _nuxt/static/1644086793/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644086793",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"07e785a8"}}}}(null,"\u002F"));
